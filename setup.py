a3b17daca50323232502cbf376c65e526befccb2
Jamie Cho <jamie@ginkgobioworks.com>
2021-04-17 16:58:59 -0400

    Pass pycodestyle tests

--- setup.py
+++ setup.py
@@ -3,76 +3,65 @@
 import setuptools
 
 # VERSION MUST be defined on line 6
-VERSION = '0.1'
+VERSION = "0.0"
 
 test_deps = [
-    'black',
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'tox',
+    "black",
+    "pycodestyle",
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "tox",
 ]
 extras = {
-    'test': test_deps,
+    "test": test_deps,
 }
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='milliluk-tools',
+    name="milliluk-tools",
     version=VERSION,
-
-    description='TRS-80 Color Computer Tools',
+    description="TRS-80 Color Computer Tools",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-
+    long_description_content_type="text/markdown",
     # The project's main homepage.
-    url='https://github.com/milliluk/milliluk-tools',
-
+    url="https://github.com/milliluk/milliluk-tools",
     # Author details
-    author='Erik Gavriluk',
-
+    author="Erik Gavriluk",
     # Choose your license
-    license='CC BY-NC-ND 4.0',
-
+    license="CC BY-NC-ND 4.0",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
     ],
-
     install_requires=[
-        'pypng',
+        "pypng",
     ],
     tests_require=test_deps,
     extras_require=extras,
-    python_requires='>=3.3',
-
+    python_requires=">=3.3",
     # What does your project relate to?
-    keywords='coco image conversion trs-80 tandy',
-
+    keywords="coco image conversion trs-80 tandy",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=setuptools.find_packages(where='src'),
+    packages=setuptools.find_packages(where="src"),
     package_dir={
-        '': 'src',
+        "": "src",
     },
-
     entry_points={
-        'console_scripts': [
-            'max2png=milliluk.max2png.max2png:main',
-            'cgp220=milliluk.cgp220.cgp220:main',
+        "console_scripts": [
+            "max2png=milliluk.max2png.max2png:main",
+            "cgp220=milliluk.cgp220.cgp220:main",
         ],
     },
 )
--- src/milliluk/cgp220/cgp220.py
+++ src/milliluk/cgp220/cgp220.py
@@ -3,9 +3,9 @@
 # emulates Tandy CGP-220 printer (Canon PJ-1080A)
 #
 # usage: cgp220.py infile.prn outfile.png
-# 
+#
 # infile.prn: captured RS-232 output
-# outfile.png: rendered as a .png 
+# outfile.png: rendered as a .png
 #
 # Copyright (C) 2015 Erik Gavriluk
 # Released under the Artistic License 2.0
@@ -14,19 +14,26 @@
 from __future__ import print_function, division
 import argparse
 import colorsys
-import png #pip install pypng
+import png  # pip install pypng
 import sys
 
 
 def main():
-    p = argparse.ArgumentParser(description='Convert CGP-220 printer output to a PNG', epilog='Copyright 2015 Erik Gavriluk. Released under the Artistic License 2.0.')
-    p.add_argument('infile', help='input file containing raw printer dump')
-    p.add_argument('outfile', help='output filename for PNG')
-    p.add_argument('-c', action="store_false", default=True, help='block cyan')
-    p.add_argument('-m', action="store_false", default=True, help='block magenta')
-    p.add_argument('-y', action="store_false", default=True, help='block yellow')
-    p.add_argument('-k', action="store_false", default=True, help='block black')
-    p.add_argument('-d', action="store", type=int, help='desaturate (0-100)')
+    p = argparse.ArgumentParser(
+        description="Convert CGP-220 printer output to a PNG",
+        epilog="Copyright 2015 Erik Gavriluk. Released under the Artistic "
+               "License 2.0.",
+    )
+    p.add_argument("infile", help="input file containing raw printer dump")
+    p.add_argument("outfile", help="output filename for PNG")
+    p.add_argument("-c", action="store_false", default=True, help="block cyan")
+    p.add_argument("-m", action="store_false", default=True,
+                   help="block magenta")
+    p.add_argument("-y", action="store_false", default=True,
+                   help="block yellow")
+    p.add_argument("-k", action="store_false", default=True,
+                   help="block black")
+    p.add_argument("-d", action="store", type=int, help="desaturate (0-100)")
 
     if len(sys.argv[1:]) == 0:
         p.print_help()
@@ -36,52 +43,52 @@ def main():
 
     width = 960
     height = 600
-    bitmap = [0] * (width*height)
+    bitmap = [0] * (width * height)
     palette = [
-        (0xff,0xff,0xff),   # white
-        (0xff,0xff,0x00),   # yellow
-        (0x7f,0x00,0xff),   # violet
-        (0xff,0x00,0x00),   # red
-        (0xff,0x00,0xff),   # magenta
-        (0x00,0xff,0x00),   # green
-        (0x00,0x00,0xff),   # blue
-        (0x00,0x00,0x00)    # black
-        ]
-    bitset = [ 128, 64, 32, 16, 8, 4, 2, 1 ]
+        (0xFF, 0xFF, 0xFF),  # white
+        (0xFF, 0xFF, 0x00),  # yellow
+        (0x7F, 0x00, 0xFF),  # violet
+        (0xFF, 0x00, 0x00),  # red
+        (0xFF, 0x00, 0xFF),  # magenta
+        (0x00, 0xFF, 0x00),  # green
+        (0x00, 0x00, 0xFF),  # blue
+        (0x00, 0x00, 0x00),  # black
+    ]
+    bitset = [128, 64, 32, 16, 8, 4, 2, 1]
 
     if arg.d is not None:
         desat = 1 - (arg.d / 100)
         for i in range(len(palette)):
             p = palette[i]
-            p = colorsys.rgb_to_hsv(p[0]/255, p[1]/255, p[2]/255)
-            p = colorsys.hsv_to_rgb(p[0], p[1]*desat, p[2]*desat)
-            palette[i] = int(p[0]*255), int(p[1]*255), int(p[2]*255)
+            p = colorsys.rgb_to_hsv(p[0] / 255, p[1] / 255, p[2] / 255)
+            p = colorsys.hsv_to_rgb(p[0], p[1] * desat, p[2] * desat)
+            palette[i] = int(p[0] * 255), int(p[1] * 255), int(p[2] * 255)
 
-    with open(arg.infile, 'rb') as file:
+    with open(arg.infile, "rb") as file:
         data = bytearray(file.read())
-        
+
         # skip to first ESC
         for i in range(len(data)):
-            if data[i] == 0x1b:
+            if data[i] == 0x1B:
                 break
-        
-        x = width-1
+
+        x = width - 1
         y = 0
 
         while i < len(data):
-            if data[i] == 0x0d:
+            if data[i] == 0x0D:
                 break
 
-            if data[i] != 0x1b and data[i+1] != 0x43 or data[i+2] != 0x4b:
-                raise Exception('sync error')
+            if data[i] != 0x1B and data[i + 1] != 0x43 or data[i + 2] != 0x4B:
+                raise Exception("sync error")
 
             i += 3
 
             for k in range(0, 75):
                 for b in bitset:
                     cc = 0 if (data[i] & b) and arg.c else 4
-                    mm = 0 if (data[i+75] & b) and arg.m else 2
-                    yy = 0 if (data[i+150] & b) and arg.y else 1
+                    mm = 0 if (data[i + 75] & b) and arg.m else 2
+                    yy = 0 if (data[i + 150] & b) and arg.y else 1
                     bitmap[y * width + x] = cc | mm | yy
                     y += 1
                 i += 1
@@ -90,7 +97,7 @@ def main():
             y = 0
             x -= 1
 
-    with open(arg.outfile, 'wb') as file:
-        img = png.Writer(width=width, height=height, bitdepth=4, palette=palette)
+    with open(arg.outfile, "wb") as file:
+        img = png.Writer(width=width, height=height, bitdepth=4,
+                         palette=palette)
         img.write_array(file, bitmap)
-
